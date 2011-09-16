167551f8aacc47b23ee81971943392fc45a6e808
cnadeau <cnadeau@360-innovations.com>
2011-09-16 10:09:50 -0400

    Permettre d'affecter null comme selected item pour forcer une sélection vide

--- com.netappsid.binding/src/com/netappsid/binding/selection/SelectionHolder.java
+++ com.netappsid.binding/src/com/netappsid/binding/selection/SelectionHolder.java
@@ -11,69 +11,76 @@ public class SelectionHolder extends AbstractSelectionModel
 {
 	private SortedSet<Integer> indexes;
 	private final List<Object> origin;
-	
+
 	public SelectionHolder(List<Object> origin, ChangeSupportFactory changeSupportFactory)
 	{
 		super(changeSupportFactory);
 		this.origin = origin;
 	}
-	
+
 	@Override
 	public SortedSet<Integer> getSelection()
 	{
 		return indexes != null ? new TreeSet<Integer>(indexes) : null;
 	}
-	
+
 	@Override
 	public boolean hasSelection()
 	{
 		return indexes != null;
 	}
-	
+
 	@Override
 	public void setSelectedItem(Object selection)
 	{
-		setSelection(origin.indexOf(selection));
+		if (selection == null)
+		{
+			setSelection();
+		}
+		else
+		{
+			setSelection(origin.indexOf(selection));
+		}
 	}
 
 	@Override
 	public void setSelection()
 	{
 		SortedSet<Integer> oldValue = indexes;
-		
+
 		indexes = null;
 		fireSelectionChange(oldValue);
 	}
-	
+
 	@Override
 	public void setSelection(Integer index)
 	{
 		SortedSet<Integer> oldValue = indexes;
-		
+
 		indexes = new TreeSet<Integer>();
 		indexes.add(index);
 		fireSelectionChange(oldValue, index);
 	}
-	
+
 	@Override
 	public void setSelection(SortedSet<Integer> indexes)
 	{
 		SortedSet<Integer> oldValue = this.indexes;
-		
+
 		this.indexes = new TreeSet<Integer>(indexes);
 		fireSelectionChange(oldValue, indexes);
 	}
-	
+
 	@Override
 	public void setSelectionInterval(Integer index0, Integer index1)
 	{
 		SortedSet<Integer> indexes = new TreeSet<Integer>();
-		
+
 		for (Integer index = index0; index <= index1; index++)
 		{
 			indexes.add(index);
 		}
-		
+
 		setSelection(indexes);
 	}
 
