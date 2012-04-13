80f50eec904e86d71690b469b698bf8a512e1204
Christian Nadeau <cnadeau>
2012-04-13 09:30:02 -0400

    ERP-2141: Changer le type de retour de l'interface

--- src/com/netappsid/binding/DynamicPresentationModelFactory.java
+++ src/com/netappsid/binding/DynamicPresentationModelFactory.java
@@ -3,6 +3,6 @@ package com.netappsid.binding;
 public interface DynamicPresentationModelFactory
 {
 
-	public abstract PresentationModel newDynamicPresentationModel(PresentationModel parentModel, String propertyName);
+	public abstract DynamicPresentationModel newDynamicPresentationModel(PresentationModel parentModel, String propertyName);
 
 }
